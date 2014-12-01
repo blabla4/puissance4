39c29085701f52d64b370e016f3b64e40ac97f26
blabla4 <amaury.cannesson@gmail.com>
2014-12-01 23:56:46 +0100

    api + angular

--- puissance4-web/src/main/java/org/isen/j2e/puissance4/HelloServlet.java
+++ puissance4-web/src/main/java/org/isen/j2e/puissance4/HelloServlet.java
@@ -1,42 +1,23 @@
 package org.isen.j2e.puissance4;
-import java.io.IOException;
-import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.lang.StringUtils;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
-    private String getHtml(String name) {
-        return "<html>"//
-                + "<body>Servlet version" //
-                + "<h1>Hello " + name + "</h1>"//
-                + "<form method='GET'>"//
-                + " <input name='name'/><input type='submit'/>"//
-                + "</form>"//
-                + "</body></html>";//
-    }
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        String name = getNameFromRequest(req);
-        out.print(getHtml(name));
-    }
-    public static String getNameFromRequest(HttpServletRequest req) {
-        String name = req.getParameter("name");
-        if (StringUtils.isNotEmpty(name)) {
-            req.getSession().setAttribute("name", name);
-        } else {
-            name = (String) req.getSession().getAttribute("name");
-            name = StringUtils.isEmpty(name) ? "World" : name;
-        }
-        return name;
+        out.print("test");
+        out.flush();
+        out.close();
+        //this.getServletContext().getRequestDispatcher( "/WEB-INF/app.jsp" ).forward( req, resp );
     }
 }
---
+++ puissance4-web/target/puissance4-web/WEB-INF/app.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>App</title>
+</head>
+<body>
+  App
+</body>
+</html>
